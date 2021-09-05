77245d07cad80ec3cee21f4335108702ab26bd3a
Bryan Breton <bryanbreton@outlook.fr>
2021-09-05 22:06:52 +0200

    feat(dropdown): two methods

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,60 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div v-for="data in dropdown_datas" :key="data.id">
+      <v-card class="pa-md-4 ma-4 mx-lg-auto">
+        <v-card-title>
+          {{ data.title }}
+        </v-card-title>
+        <v-card-text>
+          {{ data.content }}
+        </v-card-text>
+        <!-- au moment du clic on change la valeur de vision ou non -->
+        <v-btn icon @click="changeStatus(data.id)">
+          <v-icon>{{
+            dropdown_status[data.id] ? "mdi-chevron-up" : "mdi-chevron-down"
+          }}</v-icon>
+        </v-btn>
+        {{dropdown_status[data.id -1].show}}
+        <div>
+          <div v-show="dropdown_status[data.id -1].show">
+            <v-card-text>
+              ouiiiiiiiiiiiiiiiiiiiiiiiiiii
+            </v-card-text>
+          </div>
+        </div>
+      </v-card>
+    </div>
+    <v-btn @click="aide()"></v-btn>
   </div>
 </template>
+
+
+<script>
+import datas from '../../datas.json'
+export default {
+  data(){
+      return {
+        dropdown_datas: [],
+        dropdown_status: []
+      }
+    },
+    created(){
+      this.dropdown_datas = datas
+      this.dropdown_datas.forEach(() => {
+        console.log('un tour')
+        this.dropdown_status.push({show: false})
+      })
+      console.log(this.dropdown_status)
+    },
+    methods:{
+      aide(){
+        console.log(this.dropdown_status)
+      },
+      changeStatus(id){
+        console.log(this.dropdown_status[id-1].show)
+        this.dropdown_status[id-1].show = !this.dropdown_status[id-1].show
+        console.log(this.dropdown_status[id-1].show)
+      }
+    }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,39 @@
 <template>
-<div class="home"></div>
+<div class="home">
+  <v-expansion-panels>
+    <v-expansion-panel
+      v-for="item in dropdown_datas"
+      :key="item.id"
+    >
+      <v-expansion-panel-header>
+        {{item.title}}
+      </v-expansion-panel-header>
+      <v-expansion-panel-content>
+        {{item.content}}
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+  </v-expansion-panels>
+</div>
 </template>
 
 <script>
-
+  import datas from '../../datas.json'
   export default {
     name: 'Home',
-
+    data(){
+      return {
+        dropdown_datas: [],
+        dropdown_status: []
+      }
+    },
+    created(){
+      this.dropdown_datas = datas
+      this.dropdown_datas.forEach(() => {
+        console.log('un tour')
+        this.dropdown_status.push(false)
+      })
+      console.log(this.dropdown_status)
+    },
     components: {
     },
   }
