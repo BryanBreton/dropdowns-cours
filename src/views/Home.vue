30c9feb9ea57cb9093a5a148213b2d049d514bbb
LucileMarechal <lucilemarechal@gmail.com>
2021-09-06 11:09:41 +0200

    wip(dropdown):dropdown avec vuetify

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,37 @@
 <template>
-<div class="home"></div>
+<div class="home">
+  
+<v-expansion-panels>
+  <v-expansion-panel v-for="data in dropdown_datas" :key="data.id">
+<v-expansion-panel-header>
+{{data.title}}
+</v-expansion-panel-header>
+<v-expansion-panel-content>
+{{data.content}}
+</v-expansion-panel-content>
+  </v-expansion-panel>
+</v-expansion-panels>
+
+</div>
 </template>
 
 <script>
+import datas from "../../datas.json"
 
   export default {
-    name: 'Home',
-
-    components: {
-    },
+   data(){
+     return {
+       dropdown_datas : [],
+       dropdown_status : [],
+     }
+    }, 
+    created() {
+      this.dropdown_datas = datas
+      this.dropdown_datas.forEach(()=> {
+        this.dropdown_status.push({show : false})
+      })
+      console.log(this.dropdown_status)
+   }
+    
   }
 </script>
